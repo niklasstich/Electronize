68d32e61946e57e67e6299d5dc0ffe1ce020633f
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2021-12-16 13:44:03 +0100

    expand counter and test again

--- ElectronizeTest/BlazorTest.cs
+++ ElectronizeTest/BlazorTest.cs
@@ -12,12 +12,12 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void TestIncrease()
     {
         using var ctx = new Bunit.TestContext();
         var cut = ctx.RenderComponent<Counter>();
 
-        var button = cut.Find("button");
+        var button = cut.Find("button#incBtn");
         var counter = cut.Find("p");
         counter.MarkupMatches("<p>Current count: 0</p>");
         
@@ -25,5 +25,21 @@ public class Tests
         counter.MarkupMatches("<p>Current count: 42</p>");
         button.Click();
         counter.MarkupMatches("<p>Current count: 84</p>");
+    }
+
+    [Test]
+    public void TestDecrease()
+    {
+        using var ctx = new Bunit.TestContext();
+        var cut = ctx.RenderComponent<Counter>();
+        
+        var button = cut.Find("button#decBtn");
+        var counter = cut.Find("p");
+        counter.MarkupMatches("<p>Current count: 0</p>");
+        
+        button.Click();
+        counter.MarkupMatches("<p>Current count: -1</p>");
+        button.Click();
+        counter.MarkupMatches("<p>Current count: -4</p>");
     }
 }
